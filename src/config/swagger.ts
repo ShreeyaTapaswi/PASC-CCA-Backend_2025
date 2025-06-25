fc0634dadcd5bd4dafd9d13c8bfca769bf71bc66
Soham27 <phadkesoham2004@gmail.com>
2025-06-25 22:04:56 +0530

    changes in config.ts

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -10,7 +10,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://pasc-cca-backend-2025.onrender.com',
         description: 'Development server',
       },
     ],
