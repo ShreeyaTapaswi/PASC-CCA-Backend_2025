1ee803ff169a732e38a7ab462ce63a0a4a46643e
arjun <arjunsgawande123321@gmail.com>
2025-07-12 19:20:22 +0530

    signup and login integrated

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJsdoc.Options = {
     servers: [
       {
         // url: 'https://pasc-cca-backend-2025.onrender.com',
-        url:'http://localhost:3000/',
+        url:'http://localhost:4000/',
         description: 'Development server',
       },
     ],
